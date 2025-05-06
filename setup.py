a89e67e47b0d068dab4ec2b3b889b06459962c66
dyc <dyc1go@outlook.com>
2025-05-06 08:57:05 +0000

    更新包名称为"jax_pinn"并将版本号更改为1.0.1

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="compute_manager",
-    version="1.0.0",
+    name="jax_pinn",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
         "jax",
